aa3fe4ad42b5405dc85254db0740a4c2f02e77b7
eirivarn <eirik.varnes@gmail.com>
2024-07-24 14:36:34 +0200

    with write to csv

--- subscriber_test.py
+++ subscriber_test.py
@@ -3,27 +3,40 @@ This example program demonstrates how one can add a callback function to a telem
 well as how to adjust the frequency of that telemetry message, and how to remove the callback.
 """
 import time
+import csv
 import blueye.protocol as bp
 from blueye.sdk import Drone
 
+csv_writer = None
 
-def callback_depth(msg_type: str, msg: bp.Imu2Tel):
+def setup_csv():
+    global csv_writer
+    csv_file = open("telemetry_data.csv", mode="w", newline="")
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["Timesstamp", "MessageType", "IMUData"])
+
+def callback_IMU(msg_type: str, msg: bp.Imu2Tel):
     """Callback for the depth telemetry message
 
     This function is called once for every PositionEstimate message received by the telemetry watcher
     """
+    global csv_writer
+    csv_writer.writerow([time.time(), msg_type, msg.imu])
     print(f"Got a {msg_type} message with IMU data: {msg.imu}")
 
 
 if __name__ == "__main__":
+
+    setup_csv()
+
     # Instantiate a drone object
     my_drone = Drone()
 
     # Add a callback for the PositionEstimate message, storing the ID for later use
-    callback_id = my_drone.telemetry.add_msg_callback([bp.Imu1Tel], callback_depth)
+    callback_id = my_drone.telemetry.add_msg_callback([bp.Imu1Tel], callback_IMU)
 
     # Adjust the publishing frequency to 5 Hz
-    my_drone.telemetry.set_msg_publish_frequency(bp.Imu1Tel, 2)
+    my_drone.telemetry.set_msg_publish_frequency(bp.Imu1Tel, 5)
 
     # Callback is triggered by a separate thread while we sleep here
     time.sleep(5)
